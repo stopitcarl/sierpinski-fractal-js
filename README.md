02ac86b6c4d42a2d43d4ab8fd022007b77826f94
João Porto <30037641@protonmail.com>
2018-01-24 00:37:15 +0000

    Added controls to canvas

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
-An interactive display of the sierpinski fractal.
+An interactive display of the sierpinski fractal with smooth movement.
 
 To run it just open the index.html with a browser (i.e. Firefox, Chrome, Safari.....).
 
-Controls for interaction:
-
-Increase/Decrease resursion depth (number of triangles): L / K
-
-Increase/Decrease Canvas size:  I / U
-
-Toggle fullscreen: F
-  
-Decrease/Increase stroke weigth: T / Y
-  
-Decrease/Increase triangle size: G / H
-
-Decrease/Increase fading:   V / B
+Hold 'A' to see controls
+Press 'D' to toggle text
--- sierpinski.js
+++ sierpinski.js
@@ -6,9 +6,10 @@
 var canvas_scale = 0.8;
 var is_fullscreen = false; // Browser wont fullscreen as default due to permissions
 var fps = 60;
+var active_text = true;
 
 // time vars
-var time;
+var time = 0;
 var time_running = true;
 
 // triangle variables
@@ -21,20 +22,22 @@ var co;
 var si;
 var len;
 
-// triangle Constants
-var MAX_RECURRENCE = 9;
-var MIN_RECURRENCE = 0;
-
 // frame variables (modified every frame)
 var cos_t, sin_t;
 
+// triangle Constants
+var MAX_RECURRENCE = 9;
+var MIN_RECURRENCE = 0;
+var text_hint_full = "Description: Increase/Deacrease\n_________________\nNum of triangles: L / K\nCanvas size: I / U\nFullscreen: F\nStroke weight: T / Y\nTriangle size: G / H\nFading: V / B\nToggle text: D"
 
-function setup() {
+  function setup() {
   createCanvas(displayWidth*canvas_scale, displayHeight*canvas_scale);   
   strokeWeight(stroke_weight);
   noFill(); 
 
-  angle = PI/3;
+  textSize(20)
+
+    angle = PI/3;
   co = cos(angle);
   si = sin(angle);
 
@@ -44,6 +47,22 @@ function setup() {
 
 function draw() {
   background(0, 0, 0, fade_out);
+
+  // Text hint
+  if (active_text) {
+
+    noStroke();
+    if (keyIsDown(65)) { // If key "a" (65) is pressed
+      fill(200);
+      text(text_hint_full, 10, 30);
+    } else {
+      fill(100);
+      text('Press a for help', 10, 30);
+    }
+    noFill();
+  }
+
+
 
   x = width / 2;
   y = (height - (len*si))/2; 
@@ -91,45 +110,59 @@ function keyPressed() {
   case 76: // L
     recursion += recursion < MAX_RECURRENCE ? 1 : 0;
     break;
+
   case 75: // K
     recursion -= recursion > MIN_RECURRENCE ? 1 : 0;
     break;
+
   case 73: // I
     canvas_scale += canvas_scale < 1 ? 0.05 : 0;
     resizeCanvas(displayWidth * canvas_scale, displayHeight * canvas_scale);
     background(0)
-    break;
+      break;
+
   case 85: // U
     canvas_scale -= canvas_scale > 0.5 ? 0.05 : 0;
     resizeCanvas(displayWidth * canvas_scale, displayHeight * canvas_scale);
     background(0)
-    break;
+      break;
+
   case 70: // F    
     fullscreen( !fullscreen() );
     resizeCanvas(displayWidth, displayHeight);
     background(0)    
-    break;
+      break;
+
   case 84: // T
     stroke_weight += 0.5;
     strokeWeight(stroke_weight);
     break;
+
   case 89: // Y
     stroke_weight -= stroke_weight > 1 ? 0.5 : 0;
     strokeWeight(stroke_weight);
     break;
+
   case 71: // G
     triangle_scale += triangle_scale < 1 ? 0.1 : 0;
     len = displayWidth * canvas_scale * triangle_scale;
     break;
+
   case 72: // H
     triangle_scale -= triangle_scale > 0.2 ? 0.1 : 0;
     len = displayWidth * canvas_scale * triangle_scale;
     break;  
+
   case 86: // V
     fade_out += fade_out < 250 ? 5 : 0;
     break;  
+
   case 66: // B
     fade_out -= fade_out > 4 ? 5 : 0;
+    break;
+
+  case 68: // D
+    active_text = !active_text;
     break;
   }
 }
@@ -138,7 +171,7 @@ function keyPressed() {
 // Dead code : enables individual colors to triangles
 //     ( requires changes in draw function )
 /*
- 
+   
  function Triangle (x1, y1, x2, y2, x3, y3) {
  
  this.x1 = x1;
